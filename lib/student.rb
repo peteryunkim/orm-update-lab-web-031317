6e2d4aefdaed7295e38d33e231234f61c4c6d1c1
github username <github email address>
2017-03-22 16:43:53 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -75,8 +75,8 @@ class Student
   end
 
   def update
-    sql = "UPDATE students SET name = ? WHERE id = ?"
-    DB[:conn].execute(sql, name, id)
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade,  self.id)
   end
 
 end
